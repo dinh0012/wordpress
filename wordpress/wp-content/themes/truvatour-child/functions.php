58790ddec51ff4ceea35eff9917be4132f20756e
dinh0012 <dinh020304@gmail.com>
2018-05-02 21:49:13 +0700

    ad new

--- wordpress/wp-content/themes/truvatour-child/functions.php
+++ wordpress/wp-content/themes/truvatour-child/functions.php
@@ -14,86 +14,82 @@ function truvatour_child_custom_search_form(){
                             <div class="item col-md-4">
                                 <select name="departure_month" class="col-md-4">
                                     <option value="">Departure month</option>
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="5">5</option>
-                                    <option value="6">6</option>
-                                    <option value="7">7</option>
-                                    <option value="8">8</option>
-                                    <option value="9">9</option>
-                                    <option value="10">10</option>
-                                    <option value="11">11</option>
-                                    <option value="12">12</option>
+                                    <?php
+                                    for ($i = 1; $i < 12; $i++) {
+                                        ?>
+                                        <option <?php echo $i == $_GET['departure_month'] ? 'selected' : '' ?>
+                                            value="<?php echo $i?>"><?php echo $i?></option>
+                                        <?php
+                                    }
+                                    ?>
                                 </select>
                             </div>
                             <div class="item col-md-4">
                                 <select name="cruise_type" class="col-md-4">
                                     <option value="">Cruise type</option>
-                                    <option value="river">River</option>
-                                    <option value="ocean">Ocean</option>
+                                    <option <?php echo 'river' == $_GET['cruise_type'] ? 'selected' : '' ?> value="river">River</option>
+                                    <option <?php echo 'ocean' == $_GET['cruise_type'] ? 'selected' : '' ?> value="ocean">Ocean</option>
                                 </select>
                             </div>
                             <div class="item col-md-4 no-pd-r">
-                                <input type="text" name="cruise_type" class="col-md-4" placeholder="Cruise line">
+                                <input type="text" name="cruise_line" class="col-md-4" placeholder="Cruise line" value="<?php echo $_GET['cruise_line']; ?>">
                             </div>
                             <div class="item col-md-4">
                                 <select name="lengthofcruise" class="col-md-4">
                                     <option value="">Length of cruise </option>
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="5">5</option>
-                                    <option value="6">6</option>
-                                    <option value="7">7</option>
-                                    <option value="8">8</option>
-                                    <option value="9">9</option>
-                                    <option value="10">10</option>
-                                    <option value="11">11</option>
-                                    <option value="12">12</option>
-                                    <option value="13">13</option>
-                                    <option value="14">14</option>
-                                    <option value="15">15</option>
+                                    <?php
+                                    for ($i = 1; $i < 15; $i++) {
+                                        ?>
+                                            <option <?php echo $i == $_GET['lengthofcruise'] ? 'selected' : '' ?>
+                                                value="<?php echo $i?>"><?php echo $i?></option>
+                                        <?php
+                                    }
+                                    ?>
                                 </select>
                             </div>
                             <div class="item col-md-4">
                                 <select name="departure_option" class="col-md-4">
                                     <option value="">Ex-UK departure option</option>
-                                    <option value="yes">Yes</option>
-                                    <option value="no">No</option>
+                                    <option  <?php echo 'yes' == $_GET['departure_option'] ? 'selected' : '' ?>  value="yes">Yes</option>
+                                    <option <?php echo 'no' == $_GET['departure_option'] ? 'selected' : '' ?> value="no">No</option>
                                 </select>
                             </div>
                             <div class="item col-md-4 no-pd-r">
-                                <input type="text" name="departure_airport" class="col-md-4" placeholder="UK departure airport">
+                                <input type="text" name="departure_airport" class="col-md-4" placeholder="UK departure airport" value="<?php echo $_GET['departure_airport']; ?>">
                             </div>
                             <div class="item col-md-4">
                                 <select name="no_of_cabins_required" class="col-md-4">
                                     <option value="">No. of cabins required</option>
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="5">5</option>
+                                    <?php
+                                    for ($i = 1; $i < 5; $i++) {
+                                        ?>
+                                        <option <?php echo $i == $_GET['no_of_cabins_required'] ? 'selected' : '' ?>
+                                            value="<?php echo $i?>"><?php echo $i?></option>
+                                        <?php
+                                    }
+                                    ?>
                                 </select>
                             </div>
                             <div class="item col-md-4">
                                 <select name="cabin_berth" class="col-md-4">
                                     <option value="">Cabin berth</option>
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
+                                    <?php
+                                    for ($i = 1; $i < 4; $i++) {
+                                        ?>
+                                        <option <?php echo $i == $_GET['cabin_berth'] ? 'selected' : '' ?>
+                                            value="<?php echo $i?>"><?php echo $i?></option>
+                                        <?php
+                                    }
+                                    ?>
                                 </select>
                             </div>
                             <div class="item col-md-4 no-pd-r">
                                 <select name="cabin_type" class="col-md-4">
                                     <option value="">Cabin type </option>
-                                    <option value="inside">Inside</option>
-                                    <option value="outside">Outside</option>
-                                    <option value="balcony">Balcony</option>
-                                    <option value="suite">Suite</option>
+                                    <option <?php echo 'inside' == $_GET['cabin_type'] ? 'selected' : '' ?> value="inside">Inside</option>
+                                    <option <?php echo 'outside' == $_GET['cabin_type'] ? 'selected' : '' ?> value="outside">Outside</option>
+                                    <option <?php echo 'balcony' == $_GET['cabin_type'] ? 'selected' : '' ?> value="balcony">Balcony</option>
+                                    <option <?php echo 'suite' == $_GET['cabin_type'] ? 'selected' : '' ?> value="suite">Suite</option>
                                 </select>
                             </div>
                         </div>
@@ -236,17 +232,92 @@ function cruise_register_meta_boxes( $meta_boxes ) {
 }
 add_filter( 'rwmb_meta_boxes', 'cruise_register_meta_boxes' );
 
-function custom_search_query( $query ) {
-    if ($query->is_search()) {
-        // category terms search.
-        $query->set('meta_query', array(array(
-            'key' => '__meta_key__',
-            'field' => 'slug',
-            'terms' => array($_GET['product_cat_select'])),
-            'compare' => 'LIKE'
-        ));
-    }
-    return $query;
+function add_query_vars($public_query_vars) {
+    $public_query_vars[] = 'rw_departure_month';
+    $public_query_vars[] = 'rw_cruise_type';
+    $public_query_vars[] = 'rw_cruise_line';
+    $public_query_vars[] = 'rw_lengthofcruise';
+    $public_query_vars[] = 'rw_departure_option';
+    $public_query_vars[] = 'rw_departure_airport';
+    $public_query_vars[] = 'rw_no_of_cabins_required';
+    $public_query_vars[] = 'rw_cabin_berth';
+    $public_query_vars[] = 'rw_cabin_type';
+    return $public_query_vars;
 }
+add_filter('query_vars', 'add_query_vars');
 
-add_filter( 'pre_get_posts', 'custom_search_query');
+function meta_search_query($query) {
+
+    $query_args_code = array(
+        'posts_per_page' => 5,
+        'post_type' => 'page',
+        'meta_key' => 'post_title',
+        'meta_value' => $query->query_vars['s'],
+        'meta_compare' => 'LIKE'
+    );
+
+
+    $query_args_meta = array(
+        'posts_per_page' => -1,
+        'post_type' => 'page',
+        'meta_query' => array(
+            'relation' => 'OR',
+            array(
+                'key' => 'post_title',
+                'value' => $query->query_vars['s'],
+                'compare' => 'LIKE'
+            ),
+            array(
+                'key' => 'rw_departure_month',
+                'value' => $query->query_vars['departure_month'],
+                'compare' => 'LIKE'
+            ),
+            array(
+                'key' => 'rw_cruise_type',
+                'value' => $query->query_vars['cruise_type'],
+                'compare' => 'LIKE'
+            ),
+            array(
+                'key' => 'rw_cruise_line',
+                'value' => $query->query_vars['cruise_line'],
+                'compare' => 'LIKE'
+            ),
+            array(
+                'key' => 'rw_lengthofcruise',
+                'value' => $query->query_vars['lengthofcruise'],
+                'compare' => 'LIKE'
+            ),
+            array(
+                'key' => 'rw_departure_option',
+                'value' => $query->query_vars['departure_option'],
+                'compare' => 'LIKE'
+            ),
+            array(
+                'key' => 'rw_departure_airport',
+                'value' => $query->query_vars['departure_airport'],
+                'compare' => 'LIKE'
+            ),
+            array(
+                'key' => 'rw_no_of_cabins_required',
+                'value' => $query->query_vars['no_of_cabins_required'],
+                'compare' => 'LIKE'
+            ),
+            array(
+                'key' => 'rw_cabin_berth',
+                'value' => $query->query_vars['cabin_berth'],
+                'compare' => 'LIKE'
+            ),
+            array(
+                'key' => 'rw_cabin_type',
+                'value' => $query->query_vars['cabin_type'],
+                'compare' => 'LIKE'
+            ),
+
+        )
+    );
+    if ( !is_admin() && $query->is_search ) {
+        $query->set('meta_query', $query_args_meta);
+        $query->set('post_type', 'page'); // optional
+    };
+}
+add_filter( 'pre_get_posts', 'meta_search_query');
